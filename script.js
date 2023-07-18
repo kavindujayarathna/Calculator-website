53dca637b2bbbb8c19da70169bf0e67062b9b862
kavindujayarathna <kcjkavindu@gmail.com>
2023-07-18 09:07:03 +0530

    Adding test

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 function appendToResult(value) {
-    const result = document.getElementById('result');
-    result.value += value;
-  }
-  
-  function calculate() {
-    const result = document.getElementById('result');
-    const expression = result.value;
-    
-    try {
-      const answer = eval(expression);
-      result.value = answer;
-    } catch (error) {
-      result.value = 'Error';
-    }
-  }
-  
-  function clearResult() {
-    const result = document.getElementById('result');
-    result.value = '';
-  }
-  
+  const result = document.getElementById('result');
+  result.value += value;
+}
 
-  function calculate(expression) {
-    try {
-      const answer = eval(expression);
-      return answer;
-    } catch (error) {
-      return 'Error';
-    }
-  }
+function calculate() {
+  const result = document.getElementById('result');
+  const expression = result.value;
   
-  module.exports = calculate;
+  try {
+    const answer = eval(expression);
+    result.value = answer;
+  } catch (error) {
+    result.value = 'Error';
+  }
+}
+
+function clearResult() {
+  const result = document.getElementById('result');
+  result.value = '';
+}
+
+function testCalculate(expression) {
+  try {
+    const answer = eval(expression);
+    return answer;
+  } catch (error) {
+    return 'Error';
+  }
+}
+module.exports = testCalculate; 
