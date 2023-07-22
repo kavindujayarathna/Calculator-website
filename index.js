e8288911e316629de2c3adc93f7a7e21187b30ec
kavindujayarathna <kcjkavindu@gmail.com>
2023-07-22 14:40:15 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/calculate', (req, res) => {
+    const { value } = req.body;
+
+    const answer = eval(value);
+
+    return res.status(200).json({
+        status: 'success',
+        data: {
+            answer
+        }
+    });
+});
+
+app.listen(8080, () => {
+    console.log('App is listening on PORT 8080');
+});
